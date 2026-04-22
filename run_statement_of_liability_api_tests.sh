bd8bd3f407f3d6d60d4646dacaa7e1eef53f562b
himeshhp8 <214805589+himeshhp8@users.noreply.github.com>
2026-04-22 11:52:27 +0100

    Init pipeline updating for cucumber and scala test

--- run_statement_of_liability_api_tests.sh
+++ run_statement_of_liability_api_tests.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env bash
 
 environment="local"
-tags="not @ignore"
+
+cucumberTags="not @wip and not @ignore"
+scalaTestTags="\"uk.gov.hmrc.tags.WIP uk.gov.hmrc.tags.IGNORE\""
+
 if [ $# -gt 0 -a "$1" != "$environment" ];
 then
   environment="$1"
-  tags="not @wip and not @ignore"
+  cucumberTags="not @wip and not @ignore"
+  scalaTestTags="\"uk.gov.hmrc.tags.WIP uk.gov.hmrc.tags.IGNORE\""
 fi
 
-echo "*** running on $environment for tags '$tags' ***"
+echo "*** running on $environment for cucumber tags '$cucumberTags' ***"
 
-sbt -Denvironment="$environment" -Dcucumber.options="--tags '$tags'" clean 'testOnly uk.gov.hmrc.test.api.cucumber.runner.StatementOfLiabilityApiTestRunner'
+scala_exit=0
+cucumber_exit=0
+scala_test_report_dir="target/scalatest-reports"
+
+printf "\n\n\n\n*****************STARTING SCALATEST TESTS*****************\n\n"
+printf "NOTE: ScalaTest test report is available in $scala_test_report_dir\n\n"
+sbt -Denvironment="$environment" clean \
+  "testOnly uk.gov.hmrc.test.api.scalatest.specs.* -- -l $scalaTestTags -u $scala_test_report_dir" \
+  || scala_exit=$?
+
+printf "\n\n\n\n*****************STARTING CUCUMBER TESTS*****************\n\n"
+sbt -Denvironment="$environment" \
+  -Dcucumber.options="--tags '$cucumberTags'" \
+  'testOnly uk.gov.hmrc.test.api.cucumber.runner.StatementOfLiabilityApiTestRunner' \
+  || cucumber_exit=$?
+
+scala_result=$([ $scala_exit -eq 0 ] && echo "PASSED" || echo "FAILED")
+cucumber_result=$([ $cucumber_exit -eq 0 ] && echo "PASSED" || echo "FAILED")
+printf "\n\n\n\nScalaTest Tests: $scala_result | Cucumber Tests: $cucumber_result\n\n"
+
+if [ $scala_exit -ne 0 ] || [ $cucumber_exit -ne 0 ]; then
+  exit 1
+fi
