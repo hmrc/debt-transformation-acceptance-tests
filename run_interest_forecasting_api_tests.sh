640f9f4bc93c089df2c4712d68f5a0b1e3ec7c39
martin-adaramola <169259944+martin-adaramola@users.noreply.github.com>
2026-04-27 11:01:15 +0100

    added scalatest run to pipeline script and dummy test

--- run_interest_forecasting_api_tests.sh
+++ run_interest_forecasting_api_tests.sh
@@ -2,15 +2,15 @@
 
 environment="local"
 
-tags="not @wip and not @ignore"
+cucumberTags="not @wip and not @ignore"
+scalaTestTags="\"uk.gov.hmrc.tags.WIP uk.gov.hmrc.tags.IGNORE\""
 
 if [ $# -gt 0 -a "$1" != "$environment" ];
 then
   environment="$1"
-  tags="not @wip and not @ignore"
+  cucumberTags="not @wip and not @ignore"
+  scalaTestTags="\"uk.gov.hmrc.tags.WIP uk.gov.hmrc.tags.IGNORE\""
 fi
-
-echo "*** running on $environment for tags '$tags' ***"
 
 # DTD-216: Workaround to load the rule set
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:9946/ping/ping)
@@ -20,4 +20,28 @@ if [[ "$status_code" -ne 200 ]] ; then
 fi
 sleep 2
 
-sbt -Denvironment="$environment" -Dcucumber.options="--tags '$tags'" clean 'testOnly uk.gov.hmrc.test.api.cucumber.runner.InterestForecastingApiTestRunner'
+scala_exit=0
+cucumber_exit=0
+scala_test_report_dir="target/scalatest-reports"
+
+printf "\n\n\n\n*****************STARTING SCALATEST TESTS*****************\n\n"
+printf "NOTE: ScalaTest test report is available in $scala_test_report_dir\n\n"
+echo "*** running on $environment for scala tags '$scalaTestTags' ***"
+sbt -Denvironment="$environment" clean \
+  "testOnly uk.gov.hmrc.test.api.scalatest.specs.* -- -l $scalaTestTags -u $scala_test_report_dir" \
+  || scala_exit=$?
+
+printf "\n\n\n\n*****************STARTING CUCUMBER TESTS*****************\n\n"
+echo "*** running on $environment for cucumber tags '$cucumberTags' ***"
+sbt -Denvironment="$environment" \
+  -Dcucumber.options="--tags '$cucumberTags'" \
+  'testOnly uk.gov.hmrc.test.api.cucumber.runner.InterestForecastingApiTestRunner' \
+  || cucumber_exit=$?
+
+scala_result=$([ $scala_exit -eq 0 ] && echo "PASSED" || echo "FAILED")
+cucumber_result=$([ $cucumber_exit -eq 0 ] && echo "PASSED" || echo "FAILED")
+printf "\n\n\n\nScalaTest Tests: $scala_result | Cucumber Tests: $cucumber_result\n\n"
+
+if [ $scala_exit -ne 0 ] || [ $cucumber_exit -ne 0 ]; then
+  exit 1
+fi
---
+++ src/test/scala/uk/gov/hmrc/test/api/scalatest/specs/SampleSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.api.scalatest.specs
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import uk.gov.hmrc.test.api.scalatest.tags.IGNORE
+
+//Added for DTD_4428: This SampleSpec test can be removed once the real scala test in specs folder
+class SampleSpec extends AnyFlatSpec with Matchers {
+
+  "This ScalaTest" should "add two numbers correctly" in {
+    val result = 2 + 2
+    result shouldBe 5
+  }
+
+  "This ScalaTest" should "shouldn be ignored" taggedAs(IGNORE) in {
+    println("TEST 2: If this is shown, the ignore tag isn't working properly & this test has failed")
+    val result = 12512 + 3
+    result shouldBe 5
+  }
+}
